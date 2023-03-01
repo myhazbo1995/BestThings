0074ff1befa7a71d3ece3b2e2d0f9694ef2a05a7
Mykhailo Nohas <myhazbo@gmail.com>
2023-03-01 23:04:38 +0100

    Adjusted bridge to be simple bridge

--- BestThings/Bridge/Program.cs
+++ BestThings/Bridge/Program.cs
@@ -1,88 +1,50 @@
-﻿using Autofac;
+﻿
+Console.WriteLine(new Square(new VectorRenderer()).ToString());
 
 public interface IRenderer
 {
-  void RenderCircle(float radius);
+  string WhatToRenderAs { get; }
 }
 
-public class VectorRenderer : IRenderer
+public abstract class Shape
 {
-  public void RenderCircle(float radius)
+  private IRenderer rendering;
+
+  protected Shape(IRenderer rendering)
   {
-    Console.WriteLine($"Drawing a circle of radius {radius}");
+    this.rendering = rendering;
+  }
+
+  public string Name { get; set; }
+
+  public override string ToString()
+  {
+    return $"Drawing {Name} as {rendering.WhatToRenderAs}";
+  }
+}
+
+public class Triangle : Shape
+{
+  public Triangle(IRenderer strategy) : base(strategy)
+  {
+    Name = "Triangle";
+  }
+}
+
+public class Square : Shape
+{
+  public Square(IRenderer rendering) : base(rendering)
+  {
+    Name = "Square";
   }
 }
 
 public class RasterRenderer : IRenderer
 {
-  public void RenderCircle(float radius)
-  {
-    Console.WriteLine($"Drawing pixels for circle of radius {radius}");
-  }
+  public string WhatToRenderAs => "pixels";
 }
 
-public abstract class Shape
+public class VectorRenderer : IRenderer
 {
-  protected IRenderer renderer;
-
-  // a bridge between the shape that's being drawn an
-  // the component which actually draws it
-  public Shape(IRenderer renderer)
-  {
-    this.renderer = renderer;
-  }
-
-  public abstract void Draw();
-  public abstract void Resize(float factor);
-}
-
-public class Circle : Shape
-{
-  private float radius;
-
-  public Circle(IRenderer renderer, float radius) : base(renderer)
-  {
-    this.radius = radius;
-  }
-
-  public override void Draw()
-  {
-    renderer.RenderCircle(radius);
-  }
-
-  public override void Resize(float factor)
-  {
-    radius *= factor;
-  }
-}
-
-public class Demo
-{
-  static void Main(string[] args)
-  {
-    //var raster = new RasterRenderer();
-    //var vector = new VectorRenderer();
-    //var circle = new Circle(vector, 5);
-    //circle.Draw();
-    //circle.Resize(2);
-    //circle.Draw();
-
-    var cb = new ContainerBuilder();
-    cb.RegisterType<VectorRenderer>().As<IRenderer>();
-    cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(),
-      p.Positional<float>(0)));
-
-    // todo: delegate factories
-
-    // Autofac bridge
-    using (var c = cb.Build())
-    {
-      var circle = c.Resolve<Circle>(
-        new PositionalParameter(0, 5.0f)
-      );
-      circle.Draw();
-      circle.Resize(2);
-      circle.Draw();
-    }
-  }
+  public string WhatToRenderAs => "lines";
 }
